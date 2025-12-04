b355bbad0f5b7315c3a25b594380c0f9c399a7fb
ansaccar <ansaccar@student.42lyon.fr>
2025-12-04 18:43:25 +0100

    Add daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,10 @@ import reactLogo from "./react.svg";
 
 export function App() {
   return (
-    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10 bg-base-200 rounded-3xl">
       <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-24 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[spin_20s_linear_infinite]"
-        />
+        <h1>Bienvenue au village NIRD</h1>
       </div>
-
-      <h1 className="text-5xl font-bold my-4 leading-tight">Bun + React</h1>
-      <p>
-        Edit <code className="bg-[#1a1a1a] px-2 py-1 rounded font-mono">src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
     </div>
   );
 }
