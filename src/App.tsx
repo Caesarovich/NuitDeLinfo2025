0dba9b515e13288b8ec3d8b7ccd50bf462399b6a
ansaccar <ansaccar@student.42lyon.fr>
2025-12-04 18:48:35 +0100

    Add mini homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, Activity } from "react";
 import { APITester } from "./APITester";
 import "./index.css";
 
@@ -5,10 +6,22 @@ import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 
 export function App() {
+  const [page, setPage] = useState<'home' | 'snake'>('snake');
+
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10 bg-base-200 rounded-3xl">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <h1>Bienvenue au village NIRD</h1>
+      <div className="flex flex-col justify-center items-center gap-8">
+        <Activity mode={page == 'home' ? "visible" : 'hidden'}>
+          <h1>Bienvenue au village NIRD</h1>
+        </Activity>
+        
+        <Activity mode={page == 'snake' ? "visible" : 'hidden'}>
+          <h1>Bienvenue au SNAKE</h1>
+        </Activity>
+
+        <button className="btn btn-link" onClick={() => setPage('home')}>
+          Retourner au bourg du vilage
+        </button>
       </div>
     </div>
   );
